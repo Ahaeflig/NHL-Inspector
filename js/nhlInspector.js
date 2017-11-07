679e17b5c87520d440c5a535fcf8bbc773254df2
blchatel <bastien.l.chatelain@gmail.com>
2017-11-07 17:01:44 +0100

    add some d3 test and use, create placeTeam function

--- js/nhlInspector.js
+++ js/nhlInspector.js
@@ -1,6 +1,8 @@
 
 function loadTeamsData(){
 
+    let teamLoaded;
+
     $.ajax(
     {
         type: "GET",
@@ -11,10 +13,100 @@ function loadTeamsData(){
             console.log(response);
             $("#team").html(String(response.records[0].teamRecords[1].team.name));
             console.log(response.records[0].teamRecords[1].team.name);
+            teamLoaded = response;
 	    }
     );
 
-
+    return teamLoaded;
 }
 
-$( document ).ready(loadTeamsData());
+function cleanTeamsData(teamsData){
+
+    // TODO by Adan
+
+    return teamsData;
+}
+
+
+// Place a Team Logo at the middle of the corresponding SVG.
+// It addapts the logo size to fit a circle of radius svg.width/4
+function placeTeam(prefix){
+
+    const teamSVG = $('#'+prefix+'TeamSVG');
+
+    // Get the current SVG dimensions
+    const width = teamSVG.width();
+    const height = teamSVG.height();
+
+    // Compute the circle radius and center from container dimensions
+    const r = Math.min(width, height)/4;
+    const rCorr = r/2;
+    const cx = width/2;
+    const cy = height/2;
+
+    // Update the circle position and size
+    d3.select("#"+prefix+"TeamC")
+        .attr("cx", cx)
+        .attr("cy", cy)
+        .attr("r", r);
+
+    // Compute the Logo dimension and position
+    const s = r+rCorr;
+    d3.select("#"+prefix+"TeamL")
+        .attr("width", s)
+        .attr("height", s)
+        .attr("x", cx - s/2)
+        .attr("y", cy - s/2);
+}
+
+
+// When the document is ready
+$( document ).ready(
+    function(){
+        console.log("Document is Ready");
+
+        // Load the teams using ajax
+        const loadedTeams = loadTeamsData();
+        // Parse and clean the teams keeping needed data
+        const cleanedTeams = cleanTeamsData(loadedTeams);
+
+        // Everyting will be inside a SVG html element
+        // Initialize myTeamDrawing
+        const myTeamG = d3.select("#myTeam")
+            .append('svg')
+                .attr("id", "myTeamSVG")
+            .append("g")
+                .attr("id", "myTeamG");
+
+        myTeamG.append("circle")
+            .attr("id", "myTeamC")
+        myTeamG.append("image")
+            .attr("id", "myTeamL")
+            .attr("xlink:href", "logos/San_Jose_Sharks_logo.svg");
+        placeTeam("my");
+
+        // Initialize otherTeamDrawing
+        const otherTeamG = d3.select("#otherTeam")
+            .append('svg')
+                .attr("id", "otherTeamSVG")
+            .append("g")
+                .attr("id", "otherTeamG");
+
+        otherTeamG.append("circle")
+            .attr("id", "otherTeamC")
+        otherTeamG.append("image")
+            .attr("id", "otherTeamL")
+            .attr("xlink:href", "logos/Washington_Capitals_logo.svg");
+        placeTeam("other");
+
+    }
+);
+
+// When the window is resized
+$( window ).resize(
+    function() {
+        console.log("Windows is resized");
+        placeTeam("my");
+        placeTeam("other");
+    }
+);
