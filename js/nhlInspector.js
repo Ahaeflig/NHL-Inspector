e400316de25fee8aae4d9620556c560ca18ef765
Ismail Imani <ismail.imani@epfl.ch>
2017-11-07 14:44:57 +0100

    Load Data + test display team name

--- js/nhlInspector.js
+++ js/nhlInspector.js
@@ -3,22 +3,18 @@ function loadTeamsData(){
 
     $.ajax(
     {
-        type: "POST",
-		url: 'https://statsapi.web.nhl.com/api/v1/standings?expand=standings.team&season=20172018',
-		async: true,
-		data:
-        {
-	    }
+        type: "GET",
+		url: 'https://statsapi.web.nhl.com/api/v1/standings?expand=standings.team&season=20172018&date=2017-10-10'
     }).done(
         function(response){
             console.log("Data Loaded");
             console.log(response);
+            $("#team").html(String(response.records[0].teamRecords[1].team.name));
+            console.log(response.records[0].teamRecords[1].team.name);
 	    }
     );
 
 
 }
 
-$( document ).ready(function() {
-    console.log("Document is ready");
-});
+$( document ).ready(loadTeamsData());
